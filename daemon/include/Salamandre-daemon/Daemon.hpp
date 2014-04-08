180953c7cdc6861c35b6b0edb1ae674993d0f633
krozark <maxime.barbier1991@gmail.com>
2014-04-08 17:38:36 +0200

    maj

--- daemon/include/Salamandre-daemon/Daemon.hpp
+++ daemon/include/Salamandre-daemon/Daemon.hpp
@@ -6,13 +6,18 @@ namespace salamandre
     class Daemon
     {
         public:
-            Daemon();
+            /**
+             * \brief The deamon class
+             * \param listen_gui_port the port for the gui
+             * \param listen_port the port to listen the others deamon
+            */
+            Daemon(int listen_gui_port, int listen_port);
             Daemon(const Daemon&) = delete;
             Daemon& operator=(const Daemon&) = delete;
 
-        protected:
-
         private:
+            const int gui_port;
+            const int port;
     };
 }
 #endif
--- daemon/src/Salamandre-daemon/Daemon.cpp
+++ daemon/src/Salamandre-daemon/Daemon.cpp
@@ -1,8 +1,9 @@
 #include <Salamandre-daemon/Daemon.hpp>
+#include <Socket/server/server.hpp>
 
 namespace salamandre
 {
-    Daemon::Daemon()
+    Daemon::Daemon(int listen_gui_port,int listen_port): gui_port(listen_gui_port), port(listen_port)
     {
     }
 }
