14fcbbafd04f9c4fcd4c21963cadf73bf14a51ab
Marc <marc@moonscoop-fan.com>
2014-04-09 10:43:45 +0200

    [stats] compile ok :)

--- stats/include/Salamandre-stats/node.hpp
+++ stats/include/Salamandre-stats/node.hpp
@@ -1,11 +1,6 @@
 #ifndef NODE_HPP
 #define NODE_HPP
 
-#include <ORM/backends/Sqlite3.hpp>
-orm::Sqlite3Bdd def("./datas/bdd.db");
-
-orm::Bdd& orm::Bdd::Default = def;
-
 #include <ORM/fields.hpp>
 #include <ORM/models/SqlObject.hpp>
 
@@ -13,12 +8,14 @@ class Node : public orm::SqlObject<Node>
 {
 public:
     Node();
-    orm::IntegerField id;
     orm::CharField<255> host;
     orm::IntegerField port;
     orm::IntegerField last_seen_time;
 
-    MAKE_STATIC_COLUMN(id, host, port, last_seen_time);
+    MAKE_STATIC_COLUMN(host, port, last_seen_time);
 };
+
+
+#include <Salamandre-stats/node.tpl>
 
 #endif // NODE_HPP
---
+++ stats/include/Salamandre-stats/node.tpl
@@ -0,0 +1,2 @@
+// Define constructor
+REGISTER_AND_CONSTRUCT(Node, "node", host, "host", port, "port", last_seen_time, "last_seen_time");
