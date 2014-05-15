640150f379f673b478bfb26ecb3c042880cf37e8
krozark <maxime.barbier1991@gmail.com>
2014-05-15 15:16:42 +0200

    add strat saemon script

--- daemon/bin/start_daemon.sh
+++ daemon/bin/start_daemon.sh
@@ -2,8 +2,8 @@
 
 for i in {0..100..10}
 do
-    mkdir ser$i
+    mkdir -p ser$i
     cd ./ser$i
-i    ../salamandre-daemon -s `expr 2000 + $i` -g `expr 2000 + $i + 1` &>> log.txt &
+    ../salamandre-daemon -s `expr 2000 + $i` -g `expr 2000 + $i + 1` &>> log.txt &
     cd ..
 done
