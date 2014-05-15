1674397ea1a3d55b84ca44b6f98327b5c550136d
krozark <maxime.barbier1991@gmail.com>
2014-05-15 17:08:58 +0200

    maj port

--- daemon/bin/start_daemon.sh
+++ daemon/bin/start_daemon.sh
@@ -4,6 +4,6 @@ for i in {0..100..10}
 do
     mkdir -p ser$i
     cd ./ser$i
-    ../salamandre-daemon -s `expr 2000 + $i` -g `expr 2000 + $i + 1` &> log.txt &
+    ../salamandre-daemon -s `expr 20000 + $i` -g `expr 20000 + $i + 1` &> log.txt &
     cd ..
 done
